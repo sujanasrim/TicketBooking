788966ea891ca26b869aa62311bc3277d151c696
Harpreet8710 <148901217+Harpreet8710@users.noreply.github.com>
2024-09-11 15:25:53 -0400

    sixteenth commit

--- Navbar.js
+++ Navbar.js
@@ -1,1 +1,37 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './Navbar.css';
+import websitelogo from './websitelogo.jpg';
 
+function Navbar() {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleLoginClick = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  return (
+    <div>
+      <div className="navbar-container">
+        <h4 className="navbar-logo">
+          <img src={websitelogo} alt="jazzshow" className="navbar-logo-img" />
+        </h4>
+
+        <div className="navbar-menu">
+          <h4 className="navbar-home">
+            <Link to="/Mainpage">Home</Link>
+          </h4>
+          <h4 className="navbar-login" onClick={handleLoginClick}>Login</h4>
+          {showDropdown && (
+            <ul className="navbar-dropdown-menu">
+              <li><Link to="/AuthenticationPage">Login as Customer</Link></li>
+              <li><Link to="/AdminPage">Login as Admin</Link></li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
